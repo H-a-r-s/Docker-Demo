bb30cfb4af659263bbe272a6f57452cb6888bd00
Harsh Dixit <harshdixit17401@gmail.com>
2026-02-13 02:17:09 +0530

    Initial commit - Dockerized Spring Boot app

---
+++ src/main/java/com/harsh/docker_demo/DockerDemoApplication.java
@@ -0,0 +1,26 @@
+package com.harsh.docker_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello from Dockerized Spring Boot!";
+	}
+
+	@GetMapping("/health")
+	public String health() {
+		return "OK";
+	}
+
+}
